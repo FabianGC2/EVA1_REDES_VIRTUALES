dc4c4238e9f4c7f15eeb42d4f1428caff0f2ec67
FabianGC2 <fabian.garcia.cofre@estudiante.ipss.cl>
2025-07-19 00:59:14 -0400

    Agregando Archivos

---
+++ input_datos.py
@@ -0,0 +1,13 @@
+Nombre = input ("Ingrese su Nombre: ")
+Apellido = input ("Ingrese sus apellidos: ")
+Seccion = input ("Ingrese su codigo de seccion: ")
+Sede = input ("Ingrese su Institución: ")
+print (" ")
+print ("---------------------------------------------")
+print ("Hola, los datos ingresados fueron los siguentes " )
+print (" ")
+print ("Nombre completo: " + Nombre , Apellido)
+print ("Pertenece a la Seccion: " + Seccion + " del: " + Sede )
+print ("----- Gracias por ingresar sus datos -----")
+
+
